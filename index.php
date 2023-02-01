d6c7cbb3ea19c25aee46f4a64ddd93c723476055
Dee Brecke <brecke.deirdre@student.greenriver.edu>
2023-02-01 14:40:52 -0500

    Changed input type to submit to fix routing

--- index.php
+++ index.php
@@ -25,6 +25,7 @@ $f3->route('GET /', function(){
 
 //personal info page--GET loads the page, POST saves the info and reroutes
 $f3->route('GET|POST /apply1', function ($f3){
+
     //once the form is filled out, add to session array
     if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         $_SESSION['fName'] = $_POST['fName'];
@@ -33,7 +34,6 @@ $f3->route('GET|POST /apply1', function ($f3){
         $_SESSION['state'] = $_POST['state'];
         $_SESSION['phone'] = $_POST['phone'];
 
-        //this needs to reroute to experience page once I can get it all to work
         $f3->reroute('apply2');
     }
     //first rendering of the page
@@ -41,20 +41,37 @@ $f3->route('GET|POST /apply1', function ($f3){
     echo $view->render('views/personal-info.html');
 });
 
-//personal info page--GET loads the page, POST saves the info and reroutes
-$f3->route('GET|POST /apply2', function (){
+
+//experience page should work just like personal-info page
+$f3->route('GET|POST /apply2', function ($f3){
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+        $_SESSION['bio'] = $_POST['bio'];
+//        $_SESSION['github'] = $_POST['github'];
+//        $_SESSION['experience'] = $_POST['experience'];
+//        $_SESSION['relo'] = $_POST['relo'];
+
+        $f3->reroute('apply3');
+    }
+
     $view = new Template();
     echo $view->render('views/experience.html');
 });
+
+
 
 $f3->route('GET|POST /summary', function(){
     $view = new Template();
     echo $view->render('views/summary-page.html');
 });
 
-$f3->route('POST /apply3', function (){
+$f3->route('GET|POST /apply3', function ($f3){
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+        //add in the check-boxes here
+        $f3->reroute('summary');
+    }
+
     $view = new Template();
-    echo $view->render('views/summary-page.html');
+    echo $view->render('views/job-openings.html');
 });
 //Run fat free
 $f3->run();
